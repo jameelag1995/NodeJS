ed6147bae9a8e7e034f6184d240102395406f17e
jameel Agbaria <agbariajameel@gmail.com>
2023-12-24 10:56:22 +0200

    node vs browser

---
+++ NodeVsBrowser/nodevsbrowser.js
@@ -0,0 +1,9 @@
+// Q1 
+// 1- file system access: you can read and write and manipulate directories and perform file relate operations.
+// 2- network communication: nodejs allows for easy creation of servers and making http requests. it includes modules like http,https and others to handle network related tasks.
+//  3- access to system resources: nodejs can access system resources and execute commands on the server. this is often done using the child process module
+
+// Q2 
+// 1- dom manipulation: dom which represents the structure of a web page can be manipulated using client side javascript dynamically.
+// 2- user interface interactivity: client side javascript enable the creation of interactive and dynamic user interfaces. it can respond to user events such as clicks, keyboard inputs or form submission and update the ui in real time without needing to make additional requests to the server.
+// 3- browser specific features: client side js has access to browser specific features and apis such as web storage api localStorage and sessionStorage, geolocation api, canvas for drawing graphics. 
