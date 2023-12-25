090454274f0c6c43ad2ea3122899b77a7ae2ff57
jameel Agbaria <agbariajameel@gmail.com>
2023-12-25 22:57:09 +0200

    fetch api data

--- fetchApiData/app.js
+++ fetchApiData/app.js
@@ -3,12 +3,12 @@ const axios = require("axios");
 const requestUrl = "https://catfact.ninja/fact";
 const axiosUrl = "https://v2.jokeapi.dev/joke/Programming?type=single";
 
-const requireData = async () => {
+const requestData = async () => {
     const result = await request(requestUrl, (error, response, body) => {
         console.log("request fetch: \n", JSON.parse(body).fact);
     });
 };
-requireData();
+requestData();
 
 const fetchData = async () => {
     const result = await axios.get(axiosUrl);
